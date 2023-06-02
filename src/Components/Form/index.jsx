40f00bf85774705056e2f4f832958a808627d320
Henrique <you@example.com>
2023-06-02 08:32:39 -0300

    criação dos componentes e header e form com esqueletos prontos

---
+++ src/Components/Form/index.jsx
@@ -0,0 +1,18 @@
+export function Form(){
+    return(
+        <form action="">
+            <label>Descrição</label>
+            <input type="text" placeholder="Digite aqui sua descrição"/>
+            <p>Ex: Compra de roupas</p>
+
+            <label>Valor (R$)</label>
+            <input type="text" placeholder="Digite aqui o valor desejado"/>
+
+            <select name="" id="">
+                <option value="Entrada"></option>
+            </select>
+
+            <button>Inserir Valor</button>
+        </form>
+    )
+}
---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,9 @@
+import logo from "../../Assets/Nu Kenzie.svg"
+
+export function Header(){
+    return(
+        <header>
+            <img src={logo} alt="Logo da Nu kenzie - Nu em rosa e kenzie em cinza escuro" />
+        </header>
+    )
+}
